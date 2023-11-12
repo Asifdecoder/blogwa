357ef84eeb9a56b52cf8e06f101a1fb2dbb9ee1a
Asif khan <47385309+Asifdecoder@users.noreply.github.com>
2023-11-12 10:47:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 //body parser for form
 app.use(express.urlencoded({ extended: true }));
+// creating blogs array
 
 blogs = [
   {
